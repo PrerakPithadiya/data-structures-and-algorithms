ab5b608570878ab5751a7cca77f17af6643b6287
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-05-22 02:25:13 +0530

    Update Remove Nth Node From End.java

--- linked-list/Remove Nth Node From End.java
+++ linked-list/Remove Nth Node From End.java
@@ -1,16 +1,16 @@
 package linkedlist;
 
-public class RemoveNthNodeFromEnd {
+class RemoveNthNodeFromEnd {
 
     /**
      * Removes the nth node from the end of the linked list.
-     *
+     * <p>
      * This method calculates the size of the linked list and determines the node to be removed
      * by finding its position from the beginning. If the nth node from the end is the head,
      * the head is updated. Otherwise, the appropriate node is skipped.
      *
      * @param head the head of the linked list
-     * @param n the position from the end of the node to be removed (1-based index)
+     * @param n    the position from the end of the node to be removed (1-based index)
      * @return the head of the modified linked list
      */
     public Node removeNthFromEnd(Node head, int n) {
@@ -39,7 +39,7 @@ public class RemoveNthNodeFromEnd {
 
     /**
      * Calculates the size of the linked list.
-     *
+     * <p>
      * This method traverses the linked list and counts the number of nodes.
      *
      * @param head the head of the linked list
