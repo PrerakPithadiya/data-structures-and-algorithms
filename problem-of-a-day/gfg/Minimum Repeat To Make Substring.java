d13077b2474ef5e3ed9e994adc89f7e834e44797
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-11-09 01:15:16 +0530

    Update Minimum Repeat To Make Substring.java

--- problem-of-a-day/gfg/Minimum Repeat To Make Substring.java
+++ problem-of-a-day/gfg/Minimum Repeat To Make Substring.java
@@ -1,6 +1,17 @@
 
+/**
+ * Solution class to find minimum number of repetitions needed to make target string a substring
+ */
 class Solution {
 
+    /**
+     * Determines the minimum number of times the original string needs to be
+     * repeated to make the target string a substring of the repeated string.
+     *
+     * @param original The original string to be repeated
+     * @param target The target string to search for
+     * @return The minimum number of repetitions needed, or -1 if impossible
+     */
     static int minRepeats(String original, String target) {
         String repeatedString = original;
         int repeatCount = 1;
@@ -31,8 +42,12 @@ class Solution {
 
     public static void main(String[] args) {
         // Test cases
-        System.out.println(minRepeats("ww", "www")); // Output: 2
-        System.out.println(minRepeats("abcd", "cdabcdab")); // Output: 3
-        System.out.println(minRepeats("ab", "cab")); // Output: -1
+        System.out.println(minRepeats("abc", "abcabc")); // Output: 2
+        System.out.println(minRepeats("xyz", "xyzxyzxyz")); // Output: 3
+        System.out.println(minRepeats("a", "aa")); // Output: 2
+        System.out.println(minRepeats("hello", "world")); // Output: -1
+        System.out.println(minRepeats("x", "x")); // Output: 1
+        System.out.println(minRepeats("abc", "cabcabca")); // Output: 3
+        System.out.println(minRepeats("", "")); // Output: 1
     }
 }
